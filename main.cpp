a16cda80cd8110a15a47a39c688d8a2eb0e4b9de
wqq <qiqi.wu@xyzrobotics.ai>
2021-04-09 22:12:11 +0800

    add eign cmake

--- main.cpp
+++ main.cpp
@@ -4,15 +4,7 @@
 
 #include "robot_simulator/utils/log.h"
 
-int main(int, char *[]) {
-    // init logger
-    rs::Logger::get_instance().init("logger/file.txt");
-    rs::Logger::get_instance().set_logger_level(rs::LoggerLevel::trace);
-
-    LOG_ERROR("1111, {}", 1);
-    LOG_INFO("1111, {}", 1);
-    LOG_WARN("1111, {}", 1);
-    LOG_DEBUG("1111, {}", 1);
+int main(int argc, char *argv[]) {
 
     return 0;
 }
